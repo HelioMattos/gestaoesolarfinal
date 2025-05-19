c3126c8acd4972b6898958a1ea5dc2e85a3a3299
HelioMattos <heliomatto@gmail.com>
2025-05-19 12:07:48 -0300

    salvar em PDF nas duas paginas

--- projeto/professor/urls.py
+++ projeto/professor/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from professor.views import ProfessorCreateView, ProfessorListView, ProfessorUpdateView, ProfessorDeleteView
+from professor import views
 
 urlpatterns = [
     path('', ProfessorListView.as_view(), name='professor_list'),
     path('create/', ProfessorCreateView.as_view(), name='professor_create'),
     path('update/<int:pk>/', ProfessorUpdateView.as_view(), name='professor_update'),
-    path('delete/<int:pk>/', ProfessorDeleteView.as_view(), name='professor_delete')
+    path('delete/<int:pk>/', ProfessorDeleteView.as_view(), name='professor_delete'),
+    path('professor/professor/pdf/', views.gerar_pdf_professor, name='professor_pdf')
 ]
--- projeto/professor/views.py
+++ projeto/professor/views.py
@@ -3,6 +3,9 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView , UpdateView, DeleteView
 from .models import Professor
 from .forms import ProfessorForm
+from django.http import HttpResponse
+from django.template.loader import get_template
+from weasyprint import HTML
 
 # Create your views here.
 
@@ -27,3 +30,20 @@ class ProfessorDeleteView(DeleteView):
     model = Professor
     success_url = reverse_lazy('professor_list')
     template_name = 'professor/professor_confirm_delete.html'
+
+def gerar_pdf_professor(request):
+    # Pegue os dados do modelo (ou formulário, como preferir)
+    from .models import Professor
+    professor_list = Professor.objects.all()
+
+    # Renderiza o HTML como string
+    template = get_template("professor/professor_pdf.html")  # HTML próprio para PDF
+    html_string = template.render({"professor_list": professor_list})
+
+    # Gera o PDF
+    pdf_file = HTML(string=html_string).write_pdf()
+
+    # Retorna como resposta
+    response = HttpResponse(pdf_file, content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="disciplinas.pdf"'
+    return response
